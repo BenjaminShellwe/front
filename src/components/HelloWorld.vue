5aa4f54f99f98f378eb070680a6e1f7d8ffb2cf7
shellwe <benjaminshellwe@outlook.com>
2022-12-04 23:17:33 +0800

    version 1.f.1.5

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,22 +4,19 @@
       <el-col :span="6" style="margin-right: 5px; max-width: 470px;">
         <h1>{{ msg }}</h1>
         <p>
-          Spring Boot(3.0[Java17]) Vue.js<br>
+          Spring Boot(2.7[Java11]) Vue.js<br>
           日志查看工具(内部工具)<br>
           遇到问题与shellwe反馈<br>
+          目前你只能找我本人反馈
           <el-divider />
-          简易输出台：
-          <el-scrollbar height="100px">
-            输出：一切正常，因为目前没有后台<br>
-            输出：一切正常，因为目前没有后台<br>
-            输出：一切正常，因为目前没有后台<br>
-            输出：一切正常，因为目前没有后台<br>
-            输出：一切正常，因为目前没有后台<br>
-            输出：一切正常，因为目前没有后台<br>
-
-
-            情况：未链接Linux<br>
-            错误：检查网络<br>
+          <span style="font-size: 22px; ">简易输出台：</span>
+          <el-scrollbar
+              id=""
+              height="100px"
+              style="margin-top: 20px; background-color: #aaaaaa"
+              @click="getBackEndInformation"
+          >
+              此处返回LogReader的运行状态等信息
           </el-scrollbar>
         </p>
         <el-card shadow="hover">
@@ -66,13 +63,15 @@
                   <h2 style="margin: 0">日志内容</h2>
                 </el-col>
                 <el-col :span="22">
-                  <el-button>清屏</el-button>
+                  <el-button @click="getWebSocket">刷新</el-button>
                 </el-col>
               </el-row>
             </div>
           </template>
           <el-scrollbar height="600px">
-            <div v-for="o in 58" :key="o" class="text item">{{ 'List item ' + o }}</div>
+            <div id="log-container" style="background: #333; color: #aaa; padding: 10px;" >
+              此处展示日志内容 LogReader后台启动后自动刷新
+            </div>
           </el-scrollbar>
         </el-card>
       </el-col>
@@ -90,7 +89,58 @@ export default {
   },
   data() {
     return {
-      form
+      form,
+      getWebSocket
+    }
+  },
+  mounted() {
+    // WebSocket
+    if ('WebSocket' in window) {
+      this.websocket = new WebSocket('ws://localhost:8080/log')
+      // alert('连接浏览器')
+      this.initWebSocket()
+    } else {
+      alert('当前浏览器 不支持')
+    }
+  },
+  beforeUnmount() {
+    this.onbeforeunload()
+  },
+  methods: {
+    initWebSocket () {
+      // 连接错误
+      this.websocket.onerror = this.setErrorMessage
+
+      // 连接成功
+      this.websocket.onopen = this.setOnopenMessage
+
+      // 收到消息的回调
+      this.websocket.onmessage = this.setOnmessageMessage
+
+      // 连接关闭的回调
+      this.websocket.onclose = this.setOncloseMessage
+
+      // 监听窗口关闭事件，当窗口关闭时，主动去关闭websocket连接，防止连接还没断开就关闭窗口，server端会抛异常。
+      window.onbeforeunload = this.onbeforeunload
+    },
+    setErrorMessage () {
+      console.log('WebSocket连接发生错误   状态码：' + this.websocket.readyState)
+    },
+    setOnopenMessage () {
+      console.log('WebSocket连接成功    状态码：' + this.websocket.readyState)
+    },
+    setOnmessageMessage (event) {
+      // 根据服务器推送的消息做自己的业务处理
+      console.log('服务端返回：' + event.data)
+    },
+    setOncloseMessage () {
+      console.log('WebSocket连接关闭    状态码：' + this.websocket.readyState)
+    },
+    onbeforeunload () {
+      this.closeWebSocket()
+    },
+    closeWebSocket () {
+      this.websocket.close()
     }
   }
 }
@@ -104,6 +154,17 @@ const form = reactive({
   resource: '',
   desc: '',
 })
+
+
+const getWebSocket = () => {
+  let websocket = new WebSocket('ws://localhost:8080/log');
+  websocket.onmessage = function(event) {
+    console.log(event)
+    document.getElementById("log-container").innerHTML = event.data
+  }
+}
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
